5dac49ed6222dbc17102baa0289491518d8a5294
yaricelmateo <yaricel02@gmail.com>
2017-03-21 23:52:21 -0400

    sdk

---
+++ fb_init.php
@@ -0,0 +1,13 @@
+<?php
+	
+	require 'facebook-sdk/autoload.php';
+	
+	session_start();
+	
+	$fb = new Facebook\Facebook([
+  'app_id' => '190433688117642', // Replace {app-id} with your app id
+  'app_secret' => '64843113b1368c5ea3a46b8558369eb6',
+  'default_graph_version' => 'v2.8',
+  ]);
+	
+?>
